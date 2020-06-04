d26c3201de796a622962705d87199fd58db46482
Ken Whatmough <ken.whatmough@bluescape.com>
2020-06-04 11:10:54 -0400

    first run attempt

---
+++ demo/nerdybird/index.js
@@ -0,0 +1,4 @@
+const MAIN_CANVAS_ID = "main-canvas"; // by convention
+const canvas = document.getElementById(MAIN_CANVAS_ID);
+const context = canvas.getContext("2d");
+context.fillRect(10, 10, 100, 100);
