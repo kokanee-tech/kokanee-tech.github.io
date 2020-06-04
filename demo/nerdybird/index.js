071842e7a7a7351cbddb3b58acd1a3f35c6a1dd6
Ken Whatmough <ken.whatmough@bluescape.com>
2020-06-04 13:03:14 -0400

    fix #1

--- demo/nerdybird/index.js
+++ demo/nerdybird/index.js
@@ -1,4 +1,9 @@
-const MAIN_CANVAS_ID = "main-canvas"; // by convention
-const canvas = document.getElementById(MAIN_CANVAS_ID);
-const context = canvas.getContext("2d");
-context.fillRect(10, 10, 100, 100);
+try {
+  const MAIN_CANVAS_ID = "main-canvas"; // by convention
+  const canvas = document.getElementById(MAIN_CANVAS_ID);
+  const context = canvas.getContext("2d");
+  context.fillRect(10, 10, 100, 100);
+} catch (err) {
+  console.error("Execution failed: " + err.message);
+  window.alert("Opps, something went wrong.");
+}
