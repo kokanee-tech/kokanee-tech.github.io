a81e4ab6d911bdea6baa148d06927749ece0f7ab
Ken Whatmough <ken.whatmough@bluescape.com>
2020-06-05 11:50:21 -0400

    trying some things

--- demo/nerdybird/index.js
+++ demo/nerdybird/index.js
@@ -1,9 +1,31 @@
-try {
-  const MAIN_CANVAS_ID = "main-canvas"; // by convention
-  const canvas = document.getElementById(MAIN_CANVAS_ID);
-  const context = canvas.getContext("2d");
-  context.fillRect(10, 10, 100, 100);
-} catch (err) {
-  console.error("Execution failed: " + err.message);
-  window.alert("Opps, something went wrong.");
+function update() {
+  try {
+    const MAIN_CANVAS_ID = "main-canvas"; // by convention
+    const canvas = document.getElementById(MAIN_CANVAS_ID);
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    const context = canvas.getContext("2d");
+    const w = canvas.width;
+    const h = canvas.height;
+    context.beginPath();
+    context.moveTo(0, 0);
+    context.lineTo(w, 0);
+    context.lineTo(w, h);
+    context.lineTo(0, h);
+    context.lineTo(0, 0);
+    context.lineTo(w, h);
+    context.moveTo(0, h);
+    context.lineTo(w, 0);
+    context.moveTo(w / 2, 0);
+    context.lineTo(w, h / 2);
+    context.lineTo(w / 2, h);
+    context.lineTo(0, h / 2);
+    context.lineTo(w / 2, 0);
+    context.stroke();
+    requestAnimationFrame(update);
+  } catch (err) {
+    console.error("Execution failed: " + err.message);
+    window.alert("Opps, something went wrong.");
+  }
 }
+update();
