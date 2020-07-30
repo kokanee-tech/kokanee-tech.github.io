dfef5ffe21f2ff3356d2ec3d9432ad9ff8fc065c
Ken Whatmough <ken.whatmough@bluescape.com>
2020-07-30 06:34:14 -0400

    minor

--- test/Platform.spec.js
+++ test/Platform.spec.js
@@ -13,18 +13,25 @@ describe("Platform", () => {
   describe("run", () => {
     // TODO
     /*
-    with no exceptions thrown:
-    - should invoke document.getElementById once with canvas ID
-    - should invoke getContext once with a literal "2d"
-    - should invoke the callback once
-
-    with no element found for that canvas ID:
+    with no matching element found for that canvas ID:
     - should invoke console.error once
     - should invoke alert once
 
     with a callback that throws an exception:
     - should invoke console.error once
     - should invoke alert once
+
+    with no exceptions thrown:
+    - should invoke document.getElementById once with canvas ID
+    - should invoke getContext once with a literal "2d"
+    - should invoke the callback once
+
+      the dependencies passed to the callback
+      - should have an audio context
+      - should have a controls object
+      - should have a timer
+      - should have a UI element
+      - should have a visual context
     */
   });
 });
