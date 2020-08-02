13ebea10f33358247c71c1f25fcd57ef09a23cdf
Ken Whatmough <ken.whatmough@bluescape.com>
2020-08-02 08:41:36 -0400

    todo

--- demo/nerdybird/index.js
+++ demo/nerdybird/index.js
@@ -6,3 +6,10 @@ const MAIN_CANVAS_ID = "main-canvas"; // by convention
 new Platform(window).run(MAIN_CANVAS_ID, (dependencies) => {
   new App(dependencies).start();
 });
+
+/*
+TODO:
+ - a canvas-size-aware DrawingContext with efficient transforms (translate and scale) to manage stroke
+ - remove the visualContext dependency from Clock and instead pass-in the DrawingContext as a parameter to indicate()
+ - consider a better name for indicate()
+*/
