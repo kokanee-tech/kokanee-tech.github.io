1ee0c5cc150222a2f70c88dc75edbfe5994eb9d0
Ken Whatmough <ken.whatmough@bluescape.com>
2020-08-02 18:00:32 -0400

    indicator bar

---
+++ demo/nerdybird/IndicatorBar.js
@@ -0,0 +1,47 @@
+export default class IndicatorBar {
+  constructor({ visualContext }) {
+    this.deps = { visualContext };
+
+    // Default settings
+    this.settings = {
+      gaugeSize: 10,
+    };
+  }
+
+  loadSettings(settings) {
+    this.settings = { ...settings };
+    return this;
+  }
+
+  drawGauge(index, callback) {
+    const { visualContext } = this.deps;
+    const { gaugeSize } = this.settings;
+
+    visualContext.save();
+    visualContext.translate(
+      visualContext.canvas.width - (index + 0.5) * gaugeSize,
+      0.5 * gaugeSize
+    );
+    visualContext.scale(0.4 * gaugeSize, 0.4 * gaugeSize);
+    callback(visualContext);
+    visualContext.restore();
+  }
+
+  drawAngularGauge(index, angle) {
+    this.drawGauge(index, (visualContext) => {
+      visualContext.arc(0, 0, 1, 0, 2 * Math.PI);
+      visualContext.rotate(angle);
+      visualContext.moveTo(0, 0);
+      visualContext.lineTo(0, -1);
+    });
+  }
+
+  drawLinearGauge(index, value) {
+    this.drawGauge(index, (visualContext) => {
+      visualContext.moveTo(0, 1);
+      visualContext.lineTo(0, -1);
+      visualContext.moveTo(-0.2, 1 - 2 * value);
+      visualContext.lineTo(0.2, 1 - 2 * value);
+    });
+  }
+}
--- demo/nerdybird/Simulation.js
+++ demo/nerdybird/Simulation.js
@@ -1,3 +1,5 @@
+import IndicatorBar from "./IndicatorBar.js";
+
 export default class Simulation {
   constructor({ audioContext, controls, timer, visualContext }) {
     this.deps = { audioContext, controls, timer, visualContext };
@@ -6,10 +8,10 @@ export default class Simulation {
 
   start() {
     const { audioContext, controls, timer, visualContext } = this.deps;
-
-    // Simulation time
+    const indicatorBar = new IndicatorBar(this.deps).loadSettings({
+      gaugeSize: 20,
+    });
     let time = 0;
-
     /*
     const lfo = audioContext.createOscillator();
     //...more audio graph setup...
@@ -18,24 +20,13 @@ export default class Simulation {
     */
 
     timer.forEachAnimationFrame((elapsedTime) => {
-      //
-      // Use paths throughout for simple drawing
-      //
       visualContext.beginPath();
 
+      indicatorBar.drawAngularGauge(0, (2 * Math.PI * time) / 60);
+
+      // TODO: finish this...
       const canvasWidth = visualContext.canvas.width;
       const canvasHeight = visualContext.canvas.height;
-
-      // Indicate simulation time
-      visualContext.save();
-      visualContext.translate(canvasWidth - 10, 10);
-      visualContext.scale(8, 8);
-      visualContext.arc(0, 0, 1, 0, 2 * Math.PI);
-      visualContext.rotate((2 * Math.PI * time) / 60);
-      visualContext.moveTo(0, 0);
-      visualContext.lineTo(0, -1);
-      visualContext.restore();
-
       visualContext.save();
       visualContext.translate(canvasWidth / 2, canvasHeight / 2);
       visualContext.scale(100, -100);
@@ -47,16 +38,7 @@ export default class Simulation {
       } else {
         const gamepadSample = controls.getGamepadSample();
         const throttle = gamepadSample.buttons[6].value; // left trigger
-
-        // Indicate throttle
-        visualContext.save();
-        visualContext.translate(canvasWidth - 30, 10);
-        visualContext.scale(8, 8);
-        visualContext.moveTo(0, -1);
-        visualContext.lineTo(0, 1);
-        visualContext.moveTo(-0.2, 2 * throttle - 1);
-        visualContext.lineTo(0.2, 2 * throttle - 1);
-        visualContext.restore();
+        indicatorBar.drawLinearGauge(1, throttle);
 
         //lfo.frequency.value = helicopter.rotorSpeed; // or whatever...
 
@@ -71,9 +53,6 @@ export default class Simulation {
         //helicopter.update(stepsize, controls.getGamepadSample());
       }
 
-      //
-      // Render all paths after context has been restored
-      //
       visualContext.stroke();
     });
   }
