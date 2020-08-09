d15e77f9906d894a3112b7dfc6d5f329e8287a8f
Ken Whatmough <ken.whatmough@bluescape.com>
2020-08-09 07:40:36 -0400

    consistent sequence

--- test/Pseudorandom.spec.js
+++ test/Pseudorandom.spec.js
@@ -2,39 +2,45 @@ import { describe, it, expect, beforeEach } from "../src/specish/specish.js";
 import Pseudorandom from "../src/Pseudorandom.js";
 
 describe("Pseudorandom", () => {
-  let pseudorandom;
+  let pseudorandom1;
+  let pseudorandom2;
 
   beforeEach(() => {
-    pseudorandom = new Pseudorandom();
+    pseudorandom1 = new Pseudorandom();
+    pseudorandom2 = new Pseudorandom();
   });
 
   describe("constructor", () => {
     it("should set a valid initial seed", () => {
       expect(() =>
-        pseudorandom.setSeed(pseudorandom.seed)
+        pseudorandom1.setSeed(pseudorandom1.seed)
       ).not.toThrowSomething();
+    });
+
+    it("should set the same initial seed every time", () => {
+      expect(pseudorandom1.seed).toBe(pseudorandom2.seed);
     });
   });
 
   describe("setSeed", () => {
     it("should throw for a non-integer input", () => {
-      expect(() => pseudorandom.setSeed(3.14)).toThrowSomething();
+      expect(() => pseudorandom1.setSeed(3.14)).toThrowSomething();
     });
 
     it("should throw for an integer input less than 1", () => {
-      expect(() => pseudorandom.setSeed(0)).toThrowSomething();
+      expect(() => pseudorandom1.setSeed(0)).toThrowSomething();
     });
 
     it("should not throw for an input of 1", () => {
-      expect(() => pseudorandom.setSeed(1)).not.toThrowSomething();
+      expect(() => pseudorandom1.setSeed(1)).not.toThrowSomething();
     });
 
     it("should throw for an integer input greater than 2147483646", () => {
-      expect(() => pseudorandom.setSeed(2147483647)).toThrowSomething();
+      expect(() => pseudorandom1.setSeed(2147483647)).toThrowSomething();
     });
 
     it("should not throw for an input of 2147483646", () => {
-      expect(() => pseudorandom.setSeed(2147483646)).not.toThrowSomething();
+      expect(() => pseudorandom1.setSeed(2147483646)).not.toThrowSomething();
     });
   });
 
@@ -42,8 +48,19 @@ describe("Pseudorandom", () => {
     it("should always return an integer greater than 0 and less than 2147483647", () => {
       const NUM_ITERATIONS = 1000; // chosen small to be fast rather than rigorous
       for (let i = 0; i < NUM_ITERATIONS; i++) {
-        const underTest = pseudorandom.next();
-        expect(() => pseudorandom.setSeed(underTest)).not.toThrowSomething();
+        const underTest = pseudorandom1.next();
+        expect(() => pseudorandom1.setSeed(underTest)).not.toThrowSomething();
+      }
+    });
+
+    it("should return the same sequence given the same initial seed", () => {
+      const initialSeed = 1234;
+      pseudorandom1.setSeed(initialSeed);
+      pseudorandom2.setSeed(initialSeed);
+
+      const NUM_ITERATIONS = 1000; // chosen small to be fast rather than rigorous
+      for (let i = 0; i < NUM_ITERATIONS; i++) {
+        expect(pseudorandom1.next()).toBe(pseudorandom2.next());
       }
     });
   });
@@ -52,10 +69,21 @@ describe("Pseudorandom", () => {
     it("should always return a scalar value greater than or equal to 0 and less than 1", () => {
       const NUM_ITERATIONS = 1000; // chosen small to be fast rather than rigorous
       for (let i = 0; i < NUM_ITERATIONS; i++) {
-        const underTest = pseudorandom.nextScalar();
+        const underTest = pseudorandom1.nextScalar();
         expect(Number.isFinite(underTest)).toBe(true);
         expect(underTest >= 0).toBe(true);
         expect(underTest < 1).toBe(true);
+      }
+    });
+
+    it("should return the same sequence given the same initial seed", () => {
+      const initialSeed = 4321;
+      pseudorandom1.setSeed(initialSeed);
+      pseudorandom2.setSeed(initialSeed);
+
+      const NUM_ITERATIONS = 1000; // chosen small to be fast rather than rigorous
+      for (let i = 0; i < NUM_ITERATIONS; i++) {
+        expect(pseudorandom1.nextScalar()).toBe(pseudorandom2.nextScalar());
       }
     });
   });
