36502a210e28b34a6984a759a8e5b47e612c89a2
Ken Whatmough <ken.whatmough@bluescape.com>
2020-08-12 18:30:16 -0400

    todo

--- demo/nerdybird/index.js
+++ demo/nerdybird/index.js
@@ -6,3 +6,10 @@ const MAIN_CANVAS_ID = "main-canvas"; // by convention
 new Platform(window).run(MAIN_CANVAS_ID, (dependencies) => {
   new App(dependencies).start();
 });
+
+/*
+TODO: Consider a more general re-write of createGrainConvolver() that takes
+a Promise<AudioBuffer> instead of a callback to allow an OfflineAudioContext
+or the current frame-by-frame callback. Then, provide a fillBuffer() utilty
+to adapt the latter into the Promise<AudioBuffer> as appropriate.
+*/
